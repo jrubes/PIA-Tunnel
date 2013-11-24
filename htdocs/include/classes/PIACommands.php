eaa599b87b32d021c6e419abb8b05f45b27acd47
Mirko Kaiser <develop@kaisersoft.net>
2013-11-24 12:55:40 +0100

    * now defaults to origin and not origin/release_php-gui

--- htdocs/include/classes/PIACommands.php
+++ htdocs/include/classes/PIACommands.php
@@ -120,7 +120,7 @@ class PIACommands {
           $txt = '0|0';
           $this->_files->writefile($cache_file, $txt);
           return 'error checking for updates';
-    }
+      }
     }
 
     //read from cache file or get fresh info
@@ -159,7 +159,7 @@ class PIACommands {
    */
   private function get_revlist_count(){
     $ret = array();
-    exec('cd /pia ; git fetch origin &> /dev/null ; git rev-list HEAD... origin/release_php-gui --count 2> /dev/null', $ret);
+    exec('cd /pia ; git fetch origin &> /dev/null ; git rev-list HEAD... origin --count 2> /dev/null', $ret);
     if( array_key_exists(0, $ret) === true ){
       return (int)$ret[0];
     }else{
