fab70029c3d694b28351eccb1b1822cded37615f
Mirko Kaiser <develop@kaisersoft.net>
2015-05-05 13:05:00 +0200

    * this should make the wizard work again

--- htdocs/index.php
+++ htdocs/index.php
@@ -13,15 +13,16 @@ $inc_dir = './include/';
 require_once $inc_dir.'basic.php';
 
 //force setup wizard if PIA username is set to default
-//if( !VPN_is_provider_active('pia') && !VPN_is_provider_active('frootvpn') ){
-//  $_REQUEST['page'] = 'setup-wizard';
-//}
+if( $settings['HAS_BEEN_RESET'] === 'no' ){
+  $_REQUEST['page'] = 'setup-wizard';
+}
 unset($login_dta);
 
 //user account control was added later so keep the following code for the next few releases
 $settings = $_settings->get_settings();
 if( $settings['WEB_UI_USER'] == "" && $_REQUEST['page'] != 'setup-wizard'){
   $_REQUEST['page'] = 'setup-admin_account';
+
 }elseif($_REQUEST['page'] != 'setup-wizard'){
   //only allow authenticated users past this point
   if(array_key_exists('username', $_POST) !== true ){ $_POST['username'] = ''; }
