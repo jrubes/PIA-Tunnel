4367814bb17592759f53c497e92221b922d29992
Mirko Kaiser <develop@kaisersoft.net>
2015-06-19 22:49:29 +0200

    * "fix" for open-vm-tools package

--- system-update.sh
+++ system-update.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+# fix an issue caused by modifying /etc/init.d/open-vm-tools
+# added 19.06.2015 - remove after a few months
+rm /etc/init.d/open-vm-tools
+yes '' | apt-get update
+yes '' | apt-get install open-vm-tools
+
+
 # non interactive system updates
 # http://debian-handbook.info/browse/wheezy/sect.automatic-upgrades.html
 export DEBIAN_FRONTEND=noninteractive
