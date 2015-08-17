238e7af14ef301774de57e98c9e781adc959801f
Mirko Kaiser <mirko.kaiser@nolteundlauth.de>
2015-08-17 17:16:54 +0200

    * new changes to make it comptaible with BSD * removing update code for older VMs

--- system-update.sh
+++ system-update.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# fix an issue caused by modifying /etc/init.d/open-vm-tools
-# added 19.06.2015 - remove after a few months
-rm /etc/init.d/open-vm-tools
-rm /etc/rc*.d/S01open-vm-tools
-yes '' | apt-get update
-yes '' | apt-get purge open-vm-tools
-yes '' | apt-get install open-vm-tools
+
+# Debian
+if [ -f /usr/bin/apt-get ]; 
+then
+	# non interactive system updates
+	# http://debian-handbook.info/browse/wheezy/sect.automatic-upgrades.html
+	export DEBIAN_FRONTEND=noninteractive
+	yes '' | apt-get update -qq -y ; apt-get -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+fi
 
 
-# non interactive system updates
-# http://debian-handbook.info/browse/wheezy/sect.automatic-upgrades.html
-export DEBIAN_FRONTEND=noninteractive
-yes '' | apt-get update -qq -y ; apt-get -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+# FreeBSD
+if [ -f /usr/sbin/pkg ];
+then
+	pkg update
+	pkg upgrade
+fi
