244dbe339ffc932a70bd634c6d8fc3cd886efaf8
Mirko Kaiser <develop@kaisersoft.net>
2015-09-01 11:22:30 +0200

    * looks better this way

--- dev_scripts/ansible/roles/common-freebsd/templates/pia-autostart.sh
+++ dev_scripts/ansible/roles/common-freebsd/templates/pia-autostart.sh
@@ -11,22 +11,21 @@ case "$1" in
 
         # add current commit state of PIA-Tunnel
         PIAVER=`cd /usr/local/pia ; /usr/local/bin/git log -n 1 | /usr/local/bin/gawk -F" " '{print $2}' | /usr/bin/head -n 1`
-        printf "PIA-Tunnel version: $PIAVER\n\n" >> /etc/issue
+        printf "\n\nPIA-Tunnel version: $PIAVER\n\n" >> /etc/issue
 
-        echo "My IPs are" >> /etc/issue
         EM0IP=`/sbin/ifconfig em0 | /usr/bin/grep -w "inet" | /usr/local/bin/gawk -F" " '{print $2}' | /usr/bin/cut -d/ -f1`
         if [ $? -eq 0 ]; then
-            echo "em0: $EM0IP" >> /etc/issue
+            echo "em0 IP: $EM0IP" >> /etc/issue
         else
-            echo "emo: ERROR: interface not found" >> /etc/issue
+            echo "emo IP: ERROR: interface not found" >> /etc/issue
         fi
 
 
         EM1IP=`/sbin/ifconfig em1 | /usr/bin/grep -w "inet" | /usr/local/bin/gawk -F" " '{print $2}' | /usr/bin/cut -d/ -f1`
         if [ $? -eq 0 ]; then
-            echo "em1: $EM1IP" >> /etc/issue
+            echo "em1 IP: $EM1IP" >> /etc/issue
         else
-            echo "em1: interface not found" >> /etc/issue
+            echo "em1 IP: interface not found" >> /etc/issue
         fi
 
         # auto start loads default firewall rules and more....
