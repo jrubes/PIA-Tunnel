c2ad1e9306a9d9a585eafde5db15f20b7ea18675
Mirko Kaiser <develop@kaisersoft.net>
2015-09-01 15:43:28 +0200

    * developing a way to handle the firewall rules with pf

--- include/fw-pf-no-forward.sh
+++ include/fw-pf-no-forward.sh
@@ -1,60 +1,60 @@
 #!/bin/bash
-# these are the firewall settings used when the tunnel is not active.
+# generates a firewall file that may be loaded with pfctl
 LANG=en_US.UTF-8
 export LANG
 
-tcp_services = ""
-udp_services = ""
-icmp_types = "{ echoreq, unreach }"
-
-# load a default firewall
 source '/usr/local/pia/settings.conf'
-set require-order yes
 
+fwfile="/usr/local/pia/firewall/fw-no-forward"
+
+
+echo 'set require-order yes' > "$fwfile"
 
 
 #### Normalization
 #scrub provides a measure of protection against certain kinds of attacks based on incorrect handling of packet fragments
-scrub in all
+echo 'scrub in all' >> "$fwfile"
 
 # Drop incoming everything
-block in all
-pass out all keep state
+echo 'block in all' >> "$fwfile"
+echo 'pass out all keep state' >> "$fwfile"
 
 # activate spoofing protection for all interfaces
-block in quick from urpf-failed
+echo 'block in quick from urpf-failed' >> "$fwfile"
 
 
-#allo in
-pass in on em0 proto tcp from any to any port $tcp_services  keep state
-pass in on em0 proto udp from any to any port $udp_services  keep state
-pass in on em1 proto tcp from any to any port $tcp_services  keep state
-pass in on em1 proto udp from any to any port $udp_services  keep state
+#allow in
+#pass in on em0 proto tcp from any to any port $tcp_services  keep state
+#pass in on em0 proto udp from any to any port $udp_services  keep state
+#pass in on em1 proto tcp from any to any port $tcp_services  keep state
+#pass in on em1 proto udp from any to any port $udp_services  keep state
 
 
 
 #allow dhcpd traffic if enabled
 if [ "$DHCPD_ENABLED1" = 'yes' ] || [ "$DHCPD_ENABLED2" = 'yes' ]; then
-    pass in on $IF_EXT proto udp from any to any port 67,68  keep state
+    echo "pass in on $IF_EXT proto udp from any to any port 67,68  keep state" >> "$fwfile"
 
-    pass in on $IF_INT proto udp from any to any port 67,68  keep state
+    echo "pass in on $IF_INT proto udp from any to any port 67,68  keep state" >> "$fwfile"
 fi
 
 #allow dhcp traffic if interface is not static
 if [ "$IF_ETH0_DHCP" = 'yes' ]; then
-        pass out $IF_EXT proto udp from any to any port 67,68 keep state
+        echo "pass out $IF_EXT proto udp from any to any port 67,68 keep state" >> "$fwfile"
 fi
 if [ "$IF_ETH1_DHCP" = 'yes' ]; then
-        pass out $IF_INT proto udp from any to any port 67,68 keep state
+        echo "pass out $IF_INT proto udp from any to any port 67,68 keep state" >> "$fwfile"
 fi
 
 
 #allowing incoming ssh traffic
 if [ ! -z "${FIREWALL_IF_SSH[0]}" ]; then
+  printf "\n#Allow ssh traffic\n" >> "$fwfile"
+
   for interface in "${FIREWALL_IF_SSH[@]}"
   do
-    pass in on "$interface" proto tcp from any to any port 22  keep state
-    iptables -A OUTPUT -o "$interface" -m state --state RELATED,ESTABLISHED -j ACCEPT
+    echo "pass in on $interface proto tcp from any to any port 22  keep state" >> "$fwfile"
+    #iptables -A OUTPUT -o "$interface" -m state --state RELATED,ESTABLISHED -j ACCEPT
 	if [ "$VERBOSE_DEBUG" = "yes" ]; then
 		echo -e "[deb ] "$(date +"%Y-%m-%d %H:%M:%S")\
 			"- SSH has been enabled for interface: $interface"
@@ -65,10 +65,12 @@ fi
 
 #allowing incoming traffic to web UI
 if [ ! -z "${FIREWALL_IF_WEB[0]}" ]; then
+  printf "\n#Allow ssh traffic\n" >> "$fwfile"
+
   for interface in "${FIREWALL_IF_WEB[@]}"
   do
-    iptables -A INPUT -i "$interface" -p tcp --dport 80 -j ACCEPT
-    iptables -A OUTPUT -o "$interface" -m state --state RELATED,ESTABLISHED -j ACCEPT
+    echo "pass in on $interface proto tcp from any to any port 80  keep state" >> "$fwfile"
+    #iptables -A OUTPUT -o "$interface" -m state --state RELATED,ESTABLISHED -j ACCEPT
 	if [ "$VERBOSE_DEBUG" = "yes" ]; then
 		echo -e "[deb ] "$(date +"%Y-%m-%d %H:%M:%S")\
 			"- webUI has been enabled for interface: $interface"
@@ -78,4 +80,4 @@ fi
 
 
 #disable forwarding
-echo 0 > /proc/sys/net/ipv4/ip_forward
+#echo 0 > /proc/sys/net/ipv4/ip_forward
