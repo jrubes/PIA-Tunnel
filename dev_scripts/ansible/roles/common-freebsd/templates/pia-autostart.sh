50a680f20e0297bca7642bbc62d8d72ef10ae8e6
Mirko Kaiser <develop@kaisersoft.net>
2015-09-02 00:25:04 +0200

    * fixed

--- dev_scripts/ansible/roles/common-freebsd/templates/pia-autostart.sh
+++ dev_scripts/ansible/roles/common-freebsd/templates/pia-autostart.sh
@@ -13,23 +13,25 @@ case "$1" in
         PIAVER=`cd /usr/local/pia ; /usr/local/bin/git log -n 1 | /usr/local/bin/gawk -F" " '{print $2}' | /usr/bin/head -n 1`
         printf "\n\nPIA-Tunnel version: $PIAVER\n\n" >> /etc/issue
 
-        EM0IP=`/sbin/ifconfig em0 | /usr/bin/grep -w "inet" | /usr/local/bin/gawk -F" " '{print $2}' | /usr/bin/cut -d/ -f1`
+        /sbin/ifconfig em0 2> /dev/null 1> /dev/null
         if [ $? -eq 0 ]; then
+            EM0IP=`/sbin/ifconfig em0 | /usr/bin/grep -w "inet" | /usr/local/bin/gawk -F" " '{print $2}' | /usr/bin/cut -d/ -f1`
             echo "em0 IP: $EM0IP" >> /etc/issue
         else
             echo "emo IP: ERROR: interface not found" >> /etc/issue
         fi
 
 
-        EM1IP=`/sbin/ifconfig em1 | /usr/bin/grep -w "inet" | /usr/local/bin/gawk -F" " '{print $2}' | /usr/bin/cut -d/ -f1`
+        /sbin/ifconfig em1 2> /dev/null 1> /dev/null
         if [ $? -eq 0 ]; then
+            EM1IP=`/sbin/ifconfig em1 | /usr/bin/grep -w "inet" | /usr/local/bin/gawk -F" " '{print $2}' | /usr/bin/cut -d/ -f1`
             echo "em1 IP: $EM1IP" >> /etc/issue
         else
             echo "em1 IP: interface not found" >> /etc/issue
         fi
 
         # auto start loads default firewall rules and more....
-	/usr/local/pia/include/autostart.sh &
+        #/usr/local/pia/include/autostart.sh &
         exit 0
         ;;
 stop)
