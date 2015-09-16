0e7187843d5156c5393ab6ff7c4266f135f34f1e
Mirko Kaiser <develop@kaisersoft.net>
2015-09-16 21:56:06 +0200

    * fixed

--- dev_scripts/pull_latest_changes.sh
+++ dev_scripts/pull_latest_changes.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # script pulls pia_latest_changes.md from Github and copies it into the correct directory on the webserver
 # run script as cronjob on server holding pia_latest_changes.md
+# run every 3 hours
+# 0 */3 * * *      /root/pull_latest_changes.sh > /dev/null
 LANG=en_US.UTF-8
 
 mkdir -p /tmp/piatmp
 cd /tmp/piatmp
-wget https://raw.githubusercontent.com/KaiserSoft/PIA-Tunnel/release_php-gui/pia_latest_changes.md
+wget https://raw.githubusercontent.com/KaiserSoft/PIA-Tunnel/release_php-gui/pia_latest_changes.md &> /dev/null
 
-mv /tmp/piatmp/pia_latest_changes.md /var/www/htdocs/kaisersoft.net/pia_latest_changes.md
+mv /tmp/piatmp/pia_latest_changes.md /srv/www/htdocs/kaisersoft.net/pia_latest_changes.md
 
-rm /tmp/piatmp/pia_latest_changes.md
+rm /tmp/piatmp/* &> /dev/null
 rmdir /tmp/piatmp
