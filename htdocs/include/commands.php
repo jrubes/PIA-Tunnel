dc3c02b5d2c2b734bd3c0cd0247550d46f0baae0
Mirko Kaiser <develop@kaisersoft.net>
2016-03-29 00:10:57 +0200

    * paths

--- htdocs/include/commands.php
+++ htdocs/include/commands.php
@@ -1,12 +1,18 @@
 <?php
-/* 
+/*
  * define location of commands for Linux and FreeBSD
  */
 
-# BSD $CMD['sudo'] = '/usr/local/bin/sudo';
-$CMD['sudo'] = '/usr/bin/sudo';
-#$CMD['git'] = '/usr/local/bin/git';
-$CMD['git'] = '/usr/bin/git';
+if( file_exists('/usr/local/bin/gawk') )
+{
+  //FreeBSD
+  $CMD['sudo'] = '/usr/local/bin/sudo';
+  $CMD['git'] = '/usr/local/bin/git';
 
+}else{
+  $CMD['sudo'] = '/usr/bin/sudo';
+  $CMD['git'] = '/usr/bin/git';
+
+}
 
 ?>
