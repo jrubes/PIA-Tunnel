7fa3ca7305e8a1428e3cc214fb5af8a119f2daeb
Mirko Kaiser <develop@kaisersoft.net>
2016-06-18 21:42:14 +0200

    * sometimes I hate Linux shell

--- include/shutdown.sh
+++ include/shutdown.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 # shutdown or reboot the system using "at"
-# this allows the webserver to complete the request before the system begins 
-# shutting down AND using at prevents the webserver 
+# this allows the webserver to complete the request before the system begins
+# shutting down AND using at prevents the webserver
 # from hanging while executing the command
 
-
-
-if [ "$1" -eq "shutdown" ]; then
+if [ "$1" = "shutdown" ]; then
     sh -c 'echo "sleep 2 && /sbin/shutdown -P now" | at now'
 else
     sh -c 'echo "sleep 2 && /sbin/shutdown -r now" | at now'
